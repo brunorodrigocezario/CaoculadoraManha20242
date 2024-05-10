2b697db291934083a04b7526cbe2a1fa2d51df23
Bruno Cezario <bruno.cezario@ufpe.br>
2024-05-10 11:56:35 -0300

    Implementa segmented control, lógica do botão e espaçamento melhor na tela.

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -13,8 +13,11 @@ struct ContentView: View {
     @State var months: Int?
     @State var result: Int?
     
+    let portes = ["Pequeno", "Médio", "Grande"]
+    @State var porteSelecionado = "Pequeno"
+    
     var body: some View {
-        VStack(alignment: .leading){
+        VStack(alignment: .leading, spacing: 20.0){
         
             Text("Qual a idade do seu cão?")
             Text("Anos")
@@ -30,7 +33,20 @@ struct ContentView: View {
                 format: .number
             )
             Text("Porte")
+    
             // aqui vai o segmented control
+            Picker("Portes", selection: $porteSelecionado)
+            {
+                ForEach(portes, id:\.self) { porte in
+                    Text(porte)
+                }
+                
+            }
+            .pickerStyle(.segmented)
+            
+            Divider()
+            
+            Spacer()
             
             if let result {
                 Text("Seu cachorro tem, em idade humana...")
@@ -40,12 +56,12 @@ struct ContentView: View {
                     .scaledToFit()
                     .frame(maxHeight: 150)
                 .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .shadow(radius: 10)
             }
             
-            Button("Cãocular"){
-                print("cãocular")
-                result = 23
-            }
+            Spacer()
+            
+            Button("Cãocular", action: processYears)
             .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
             .frame(height: 50)
             .background(.indigo)
@@ -57,6 +73,39 @@ struct ContentView: View {
         .textFieldStyle(.roundedBorder)
         .keyboardType(.numberPad)
         .padding()
+    }
+    
+    func processYears() {
+        print("cãocular")
+        
+        guard let years, let months else {
+            print("campos não preenchidos")
+            return
+        }
+        
+        guard months > 0 || years > 0 else {
+            print("pelo menos um campo tem ser maior que zero")
+            return
+        }
+        
+        // o resultado vai ser os anos * multiplicador + a fração do ano em meses * multiplicador
+        // multiplicador:
+        //  * pequeno: 6
+        //  * médio: 7
+        //  * grande: 8
+        let multiplicador: Int
+        switch porteSelecionado {
+        case "Pequeno":
+            multiplicador = 6
+        case "Médio":
+            multiplicador = 7
+        case "Grande":
+            multiplicador = 8
+        default:
+            multiplicador = 0
+        }
+        
+        result = years * multiplicador + months * multiplicador / 12
     }
 }
 
