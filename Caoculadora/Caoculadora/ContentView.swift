2a87c3d33fc19d9cf5b8298d2a5816b3a7113801
Bruno Cezario <bruno.cezario@ufpe.br>
2024-05-13 12:00:12 -0300

    Cria enum de Porte, muda lógica de criação do picker, e implementa style guide para fontess

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -15,32 +15,37 @@ struct ContentView: View {
     
     let portes = ["Pequeno", "Médio", "Grande"]
     @State var porteSelecionado = "Pequeno"
+    @State var porteSelected = Porte.pequeno
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20.0){
         
             Text("Qual a idade do seu cão?")
+                .font(.header5)
             Text("Anos")
+                .font(.body1)
             TextField(
                 "Quantos anos completos tem seu cão",
                 value: $years,
                 format: .number
-            )
+            ) .font(.body1)
             Text("Meses")
+                .font(.body1)
             TextField(
                 "E quantos meses além disso ele tem?",
                 value: $months,
                 format: .number
             )
             Text("Porte")
-    
+                .font(.body1)
+            
+            //MARK: - PICKER
             // aqui vai o segmented control
-            Picker("Portes", selection: $porteSelecionado)
+            Picker("Portes", selection: $porteSelected)
             {
-                ForEach(portes, id:\.self) { porte in
-                    Text(porte)
+                ForEach(Porte.allCases, id:\.self) { porte in
+                    Text(porte.rawValue) //p associar que o porte/enum é string
                 }
-                
             }
             .pickerStyle(.segmented)
             
@@ -50,19 +55,22 @@ struct ContentView: View {
             
             if let result {
                 Text("Seu cachorro tem, em idade humana...")
+                    .font(.body1)
                 Text("\(result) anos")
+                    .font(.display)
             } else {
                 Image(ImageResource.clarinha).resizable()
                     .scaledToFit()
                     .frame(maxHeight: 150)
-                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .frame(maxWidth: .infinity)
                 .shadow(radius: 10)
             }
             
             Spacer()
             
             Button("Cãocular", action: processYears)
-            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .font(.body1)
+            .frame(maxWidth: .infinity)
             .frame(height: 50)
             .background(.indigo)
             .foregroundStyle(.white)
@@ -94,15 +102,13 @@ struct ContentView: View {
         //  * médio: 7
         //  * grande: 8
         let multiplicador: Int
-        switch porteSelecionado {
-        case "Pequeno":
+        switch porteSelected {
+        case .pequeno:
             multiplicador = 6
-        case "Médio":
+        case .medio:
             multiplicador = 7
-        case "Grande":
+        case .grande:
             multiplicador = 8
-        default:
-            multiplicador = 0
         }
         
         result = years * multiplicador + months * multiplicador / 12
---
+++ Caoculadora/Caoculadora/Porte.swift
@@ -0,0 +1,15 @@
+//
+//  Porte.swift
+//  Caoculadora
+//
+//  Created by Bruno Cezario on 13/05/24.
+//
+
+import Foundation
+
+enum Porte: String, CaseIterable {
+    case pequeno = "Pequeno"
+    case medio = "Médio"
+    case grande = "Grande"
+}
+
