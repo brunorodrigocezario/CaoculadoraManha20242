a66b686df1a64e76e86f4c5b2e7f90e9eaaef579
Bruno Cezario <bruno.cezario@ufpe.br>
2024-05-14 09:17:32 -0300

    Modifica o enum para organizar o codigo

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -6,115 +6,85 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
-    
-    @State var years: Int?
-    @State var months: Int?
-    @State var result: Int?
-    
-    let portes = ["Pequeno", "Médio", "Grande"]
-    @State var porteSelecionado = "Pequeno"
-    @State var porteSelected = Porte.pequeno
-    
-    var body: some View {
-        VStack(alignment: .leading, spacing: 20.0){
-        
-            Text("Qual a idade do seu cão?")
-                .font(.header5)
-            Text("Anos")
-                .font(.body1)
-            TextField(
-                "Quantos anos completos tem seu cão",
-                value: $years,
-                format: .number
-            ) .font(.body1)
-            Text("Meses")
-                .font(.body1)
-            TextField(
-                "E quantos meses além disso ele tem?",
-                value: $months,
-                format: .number
-            )
-            Text("Porte")
-                .font(.body1)
-            
-            //MARK: - PICKER
-            // aqui vai o segmented control
-            Picker("Portes", selection: $porteSelected)
-            {
-                ForEach(Porte.allCases, id:\.self) { porte in
-                    Text(porte.rawValue) //p associar que o porte/enum é string
-                }
-            }
-            .pickerStyle(.segmented)
-            
-            Divider()
-            
-            Spacer()
-            
-            if let result {
-                Text("Seu cachorro tem, em idade humana...")
-                    .font(.body1)
-                Text("\(result) anos")
-                    .font(.display)
-            } else {
-                Image(ImageResource.clarinha).resizable()
-                    .scaledToFit()
-                    .frame(maxHeight: 150)
-                .frame(maxWidth: .infinity)
-                .shadow(radius: 10)
-            }
-            
-            Spacer()
-            
-            Button("Cãocular", action: processYears)
-                .font(.body1)
-            .frame(maxWidth: .infinity)
-            .frame(height: 50)
-            .background(.indigo)
-            .foregroundStyle(.white)
-            .clipShape(.rect(cornerRadius: 10))
-         //   .clipShape(.rect(cornerRadius: 10))
-        
+  @State var years: Int?
+  @State var months: Int?
+  @State var result: Int?
+  let portes = ["Pequeno", "Médio", "Grande"]
+  @State var porteSelecionado = "Pequeno"
+  @State var porteSelected = Porte.pequeno
+  var body: some View {
+    VStack(alignment: .leading, spacing: 20.0){
+      Text("Qual a idade do seu cão?")
+        .font(.header5)
+      Text("Anos")
+        .font(.body1)
+      TextField(
+        "Quantos anos completos tem seu cão",
+        value: $years,
+        format: .number
+      ) .font(.body1)
+      Text("Meses")
+        .font(.body1)
+      TextField(
+        "E quantos meses além disso ele tem?",
+        value: $months,
+        format: .number
+      )
+      Text("Porte")
+        .font(.body1)
+      //MARK: - PICKER
+      // aqui vai o segmented control
+      Picker("Portes", selection: $porteSelected)
+      {
+        ForEach(Porte.allCases, id:\.self) { porte in
+          Text(porte.rawValue) //p associar que o porte/enum é string
         }
-        .textFieldStyle(.roundedBorder)
-        .keyboardType(.numberPad)
-        .padding()
+      }
+      .pickerStyle(.segmented)
+      Divider()
+      Spacer()
+      if let result {
+        Text("Seu cachorro tem, em idade humana...")
+          .font(.body1)
+        Text("\(result) anos")
+          .font(.display)
+      } else {
+        Image(ImageResource.clarinha).resizable()
+          .scaledToFit()
+          .frame(maxHeight: 150)
+          .frame(maxWidth: .infinity)
+          .shadow(radius: 10)
+      }
+      Spacer()
+      Button("Cãocular", action: processYears)
+        .font(.body1)
+        .frame(maxWidth: .infinity)
+        .frame(height: 50)
+        .background(.indigo)
+        .foregroundStyle(.white)
+        .clipShape(.rect(cornerRadius: 10))
+      //  .clipShape(.rect(cornerRadius: 10))
     }
-    
-    func processYears() {
-        print("cãocular")
-        
-        guard let years, let months else {
-            print("campos não preenchidos")
-            return
-        }
-        
-        guard months > 0 || years > 0 else {
-            print("pelo menos um campo tem ser maior que zero")
-            return
-        }
-        
-        // o resultado vai ser os anos * multiplicador + a fração do ano em meses * multiplicador
-        // multiplicador:
-        //  * pequeno: 6
-        //  * médio: 7
-        //  * grande: 8
-        let multiplicador: Int
-        switch porteSelected {
-        case .pequeno:
-            multiplicador = 6
-        case .medio:
-            multiplicador = 7
-        case .grande:
-            multiplicador = 8
-        }
-        
-        result = years * multiplicador + months * multiplicador / 12
+    .textFieldStyle(.roundedBorder)
+    .keyboardType(.numberPad)
+    .padding()
+  }
+  func processYears() {
+    print("cãocular")
+    guard let years, let months else {
+      print("campos não preenchidos")
+      return
     }
+    guard months > 0 || years > 0 else {
+      print("pelo menos um campo tem ser maior que zero")
+      return
+    }
+    result = porteSelected.calcularIdade(deAnos: years,
+                       eMeses: months)
+  }
 }
-
-#Preview {
+  #Preview {
     ContentView()
-}
+  }
+
